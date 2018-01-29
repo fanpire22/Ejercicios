b1ba3e00537f43fe01b91e81b1cc2f2ef10b769f
Fanpy <Fanpire22@yahoo.es>
2018-01-29 18:17:41 +0100

    Firstday2d

---
+++ FirstDay2D/Assets/GameAssets/_Scripts/Zombie.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float SpeedMovement;
+    Animator _animator;
+    Rigidbody2D _rb;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        _animator.SetFloat("Speed", Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            _animator.SetFloat("Horizontal", horizontal);
+            _animator.SetFloat("Vertical", vertical);
+
+            Vector2 direction = new Vector2(horizontal, vertical);
+            _rb.MovePosition(_rb.position + direction * Time.deltaTime * SpeedMovement);
+        }
+    }
+}
