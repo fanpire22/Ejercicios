16666382343de90b81d125427be4c3dac4fe25da
Fanpy <Fanpire22@yahoo.es>
2018-02-14 21:08:37 +0100

    Simon salta. Y tal

--- Practica2/Assets/_Assets/Scripts/Characters/BaseCharacter.cs
+++ Practica2/Assets/_Assets/Scripts/Characters/BaseCharacter.cs
@@ -2,9 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCharacter : MonoBehaviour {
+public class BaseCharacter : MonoBehaviour
+{
 
+    [SerializeField] [Range(1, 10)] protected float _speedMovement;
+
+    public Animator ani { get; protected set; }
+    public Rigidbody2D rig { get; protected set; }
     public HealthManager HealthManager { get; protected set; }
+    public SpriteRenderer spr { get; protected set; }
+
+#if UNITY_EDITOR
+    [Header("Develop")]
+    [SerializeField] bool bDrawDebug;
+
+    protected virtual void DrawDebug()
+    {
+
+    }
+
+#endif
+
+    protected void OnDrawGizmos()
+    {
+
+#if UNITY_EDITOR
+        if (bDrawDebug) DrawDebug();
+#endif
+    }
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+    }
 
     protected void Move(Vector2 direction)
     {
@@ -13,7 +45,8 @@ public class BaseCharacter : MonoBehaviour {
 
     protected void SimpleMove(Vector2 direction)
     {
-
+        rig.velocity= new Vector2(direction.x, rig.velocity.y);
+        ani.SetFloat("Speed", Mathf.Abs(direction.x));
     }
 
     protected void Jump()
--- Practica2/Assets/_Assets/Scripts/Characters/SimonBelmont.cs
+++ Practica2/Assets/_Assets/Scripts/Characters/SimonBelmont.cs
@@ -4,23 +4,71 @@ using UnityEngine;
 
 public class SimonBelmont : BaseCharacter
 {
+    [Header("Simon Simon!")]
+    [SerializeField]
+    private LayerMask _jumpingLayer;
+    [SerializeField] private float _detectionRadius;
+    [SerializeField] private float _jumpForce;
+
     private float _horizontalAxis;
     private bool _bJumpPressed;
     private bool _bAttackPressed;
+    private bool _bInAir;
 
     private Collider2D[] _footDetection = new Collider2D[5];
-    private bool _bInAir;
+
+    void UpdateJump()
+    {
+        base.ani.SetBool("OnAir", _bInAir);
+
+        //Obtenemos la información sobre el estado de la animación del inicio del salto.
+        AnimatorStateInfo state = ani.GetCurrentAnimatorStateInfo(0);
+
+        //Estamos animando el salto y está a punto de terminar la animación
+        if (state.IsName("Jump.JumpStart") && state.normalizedTime > 0.9f)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, _jumpForce);
+        }
+
+    }
 
     protected override void Update()
     {
         base.Update();
+
+        UpdateJump();
+
 
         _horizontalAxis = Input.GetAxis("Horizontal");
         _bJumpPressed = Input.GetButton("Jump");
         _bAttackPressed = Input.GetButton("Fire1");
 
         //Detectamos suelo por medio de una esfera ubicada en el punto de pivote (pies)
-        Physics2D.OverlapCircleNonAlloc(transform.position,0.2f,_footDetection);
+        int count = Physics2D.OverlapCircleNonAlloc(transform.position, _detectionRadius, _footDetection, _jumpingLayer);
+        _bInAir = count == 0;
+
+        //Salto
+        if (_bJumpPressed && !_bInAir)
+        {
+            base.ani.SetTrigger("Jump");
+        }
+
+        //Movimiento
+        SimpleMove(Vector2.right * _horizontalAxis * _speedMovement);
+        if (_horizontalAxis != 0)
+        {
+            spr.flipX = _horizontalAxis > 0;
+        }
     }
 
+
+#if UNITY_EDITOR
+
+    protected override void DrawDebug()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, _detectionRadius);
+    }
+
+#endif
 }
