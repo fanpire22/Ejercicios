0971012542497a67c8fd0461d6d7c5f17fd74b52
Fanpy <Fanpire22@yahoo.es>
2018-02-15 21:10:41 +0100

    Añadimos partes de la escena aditiva de tienda

---
+++ Practica2/Assets/_Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("References")]
+    [SerializeField] SimonBelmont _simonSimon;
+
+    //SingleTonTORRON
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        GameManager.instance = this;
+    }
+}
---
+++ Practica2/Assets/_Assets/Scripts/StoreItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreItem : MonoBehaviour {
+
+    [SerializeField] Image _icon;
+    [SerializeField] Text _name;
+    [SerializeField] Text _cost;
+    [SerializeField] Button _buyButton;
+
+    public void Initialize(Sprite Image, string Name, int Cost)
+    {
+        _icon.sprite = Image;
+        _name.text = Name;
+        _name.text = string.Format("{0}G",Cost);
+    }
+
+}
