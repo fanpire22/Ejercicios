e38b2dba0bb5aa78e760b41c0cda404fde704c8d
Fanpy <Fanpire22@yahoo.es>
2018-02-19 21:15:10 +0100

    Diversión con cámaras

---
+++ Practica2/Assets/_Assets/Scripts/Stores/SellerTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellerTemplate : MonoBehaviour
+{
+    bool bOpenStore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !bOpenStore)
+        {
+            bOpenStore = StoreWindow.LoadStore();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && bOpenStore)
+        {
+            bOpenStore = StoreWindow.unloadStore();
+        }
+    }
+}
---
+++ Practica2/Assets/_Assets/Scripts/Stores/StoreWindow.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoreWindow : MonoBehaviour
+{
+    [System.Serializable]
+    public struct FStoreItem
+    {
+        Sprite sprite;
+        string name;
+        int amount;
+    }
+
+    static bool bOpenStore;
+
+    //Singleton
+    private static StoreWindow _instance;
+    public static StoreWindow Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                StoreWindow.LoadStore();
+                return _instance;
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    /// <summary>
+    /// Función que abre una escena aditiva de tienda
+    /// </summary>
+    /// <returns>El estado de la escena</returns>
+    public static bool LoadStore()
+    {
+        if (!bOpenStore)
+        {
+            SceneManager.LoadScene("Store", LoadSceneMode.Additive);
+            bOpenStore = true;
+        }
+        return bOpenStore;
+    }
+
+    /// <summary>
+    /// Función que cierra una escena aditiva de tienda
+    /// </summary>
+    /// <returns></returns>
+    public static bool unloadStore()
+    {
+        if (bOpenStore)
+        {
+            _instance.StartCoroutine(_instance.unloadStore_Corrutine());
+            bOpenStore = false;
+        }
+        return bOpenStore;
+    }
+
+    public IEnumerator unloadStore_Corrutine()
+    {
+        AsyncOperation op = SceneManager.UnloadSceneAsync("Store");
+        yield return op;
+    }
+
+}
