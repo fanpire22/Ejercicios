b7df1653bfb2f6e8a41eba50dd3697c226425811
Fanpy <Fanpire22@yahoo.es>
2018-03-08 23:38:40 +0100

    Control de escenas, repaso etc.

---
+++ Practica2/Assets/_Assets/Scripts/Characters/DragonCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonCharacter : BaseCharacter {
+    protected override void OnAttack()
+    {
+
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        Transform playerTransform = GameManager.instance.getSimonSimon().transform;
+
+    }
+    public override void OnDeath()
+    {
+        base.OnDeath();
+    }
+}
---
+++ Practica2/Assets/_Assets/Scripts/Managers/TransitionLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class TransitionLevel : MonoBehaviour {
+
+    [SerializeField] string _targetScene;
+    [SerializeField] Vector3 FinalLocation;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        SceneManager.LoadSceneAsync(_targetScene,LoadSceneMode.Additive);
+    }
+
+    IEnumerator LoadScene_Corrutine()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(_targetScene, LoadSceneMode.Additive);
+        ao.completed += Ao_completed;
+        yield return new WaitUntil(() => { return ao.isDone; }) ;
+    }
+
+    private void Ao_completed(AsyncOperation obj)
+    {
+        GameManager.instance.getSimonSimon().rig.MovePosition(FinalLocation);
+    }
+}
