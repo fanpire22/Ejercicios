05b52260ef53c20dac52a0ce95b395596ea284ef
Fanpy <Fanpire22@yahoo.es>
2018-03-10 18:31:34 +0100

    Inserción de los últimos ejercicios de clase

--- Practica2/Assets/_Assets/Scripts/Characters/DragonCharacter.cs
+++ Practica2/Assets/_Assets/Scripts/Characters/DragonCharacter.cs
@@ -2,21 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragonCharacter : BaseCharacter {
-    protected override void OnAttack()
-    {
+public class DragonCharacter : BaseCharacter
+{
+    [SerializeField] float _minDistanceAttack;
+    [SerializeField] Fireball _fireball;
+    [SerializeField] Transform _spawn1;
+    [SerializeField] Transform _spawn2;
 
-    }
+    Vector3 _direction;
+    bool _bInAttackBehaviour;
 
     protected override void Update()
     {
         base.Update();
 
         Transform playerTransform = GameManager.instance.getSimonSimon().transform;
-
+        _direction = playerTransform.position - transform.position;
+        _bInAttackBehaviour = _direction.magnitude < _minDistanceAttack;
     }
+
+    protected override void OnAttack()
+    {
+    }
+
     public override void OnDeath()
     {
         base.OnDeath();
+        GameManager.instance.getSimonSimon().GetInventory().AddGold(100);
+        Destroy(gameObject);
+    }
+
+    public void Spawn1()
+    {
+        if(_bInAttackBehaviour)
+        {
+            Fireball fireball = Instantiate(_fireball, _spawn1.transform.position, Quaternion.identity);
+            fireball.Initialize(gameObject);
+            fireball.DirectionFireball = _direction.normalized * 3;
+        }
+    }
+
+    public void Spawn2()
+    {
+        if (_bInAttackBehaviour)
+        {
+            Fireball fireball = Instantiate(_fireball, _spawn2.transform.position, Quaternion.identity);
+            fireball.Initialize(gameObject);
+            fireball.DirectionFireball = _direction.normalized * 3;
+        }
     }
 }
--- Practica2/Assets/_Assets/Scripts/Weapons/FireBall.cs
+++ Practica2/Assets/_Assets/Scripts/Weapons/FireBall.cs
@@ -2,44 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBall : WeaponBase {
-
-    public Vector3 Target;
-    public float TorqueSpeed = 80;
-
-    private float _fuel = 100;
-    private float _consumptionOnMove = 1;
-    private float _consumptionOnHit = 10;
-
-    FDamageWeapon dmg = new FDamageWeapon()
-    {
-        amount = 1,
-        type = EDamageTypes.Fire
-    };
-
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
-        transform.position = Target * Time.deltaTime;
-        transform.Rotate(Vector3.forward * TorqueSpeed * Time.deltaTime);
-        _fuel -= _consumptionOnMove;
-    }
-
+public class Fireball : WeaponBase
+{
+    public Vector3 DirectionFireball { get; set; }
+    private float Fuel = 100;
+    
 
     protected override void OnHealthOverlap(List<HealthManager> colliders)
     {
         base.OnHealthOverlap(colliders);
 
-        //Perdemos combustible por golpe
-        for (int i = 0; i < colliders.Count && _fuel > 0; i++)
+        for(int i = 0; i < colliders.Count && Fuel > 0; i++)
         {
-            _fuel -= _consumptionOnHit;
-            colliders[i].Damage(dmg);
+            //FDamageWeapon damage = new FDamageWeapon()
+            //{
+            //    AmmountDamage = 1,
+            //    Type = EDamageTypes.Bludgeoning
+            //};
+            colliders[i].Damage(this);
+            Fuel -= 10;
         }
-        if (_fuel < 0)
-        {
-            Destroy(gameObject);
-        }
+
+        Destroy(gameObject);
     }
+
+    protected override void Update()
+    {
+        base.Update();
+        transform.position += DirectionFireball * Time.deltaTime;
+        transform.Rotate(Vector3.forward * 80 * Time.deltaTime);
+    }
+
+    
 }
