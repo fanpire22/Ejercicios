05791d27c91bf739b1c73258d02c4bc66dcde135
Fanpy <Fanpire22@yahoo.es>
2018-04-03 20:23:11 +0200

    Anime Edition

---
+++ PruebasMecanim/Assets/GameAssets/Scripts/ParticlesDuration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesDuration : MonoBehaviour {
+
+    Animator _animator;
+    [SerializeField] ParticleSystem _particles;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+	void Update () {
+        float particlesDuration = _animator.GetFloat("particlesDuration");
+                
+        ParticleSystem.MainModule mainModule = _particles.main;
+        mainModule.startLifetimeMultiplier = particlesDuration;
+        
+	}
+}
