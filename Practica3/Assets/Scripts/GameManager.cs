2900f6978c2b5a644d7e94bae7a5a49a3bc37c7c
Fanpy <fanpire22@yahoo.es>
2018-04-09 21:09:39 +0200

    Empezamos nueva práctica

---
+++ Practica3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    static GameManager _instance;
+    public static GameManager GetInstance
+    {
+        get { return _instance; }
+    }
+
+    public Player player
+    {
+        get; private set;
+    }
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+
+        _instance = this;
+    }
+
+}
---
+++ Practica3/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+
+    NavMeshAgent _agent;
+
+    public void GoToDestination(Vector3 destination)
+    {
+        _agent.SetDestination(destination);
+    }
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+}
